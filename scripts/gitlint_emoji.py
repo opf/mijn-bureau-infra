0b8767af2284e4097175fb93a05a5d8295052b79
Berry den Hartog <38954346+berrydenhartog@users.noreply.github.com>
2025-08-02 15:49:00 +0200

    🐛(fix) add scope check in commit message

--- scripts/gitlint_emoji.py
+++ scripts/gitlint_emoji.py
@@ -7,7 +7,6 @@ from __future__ import unicode_literals
 import re
 
 import requests
-
 from gitlint.rules import CommitMessageTitle, LineRule, RuleViolation
 
 
@@ -31,7 +30,14 @@ class GitmojiTitle(LineRule):
             "https://raw.githubusercontent.com/carloscuesta/gitmoji/master/packages/gitmojis/src/gitmojis.json"
         ).json()["gitmojis"]
         emojis = [item["emoji"] for item in gitmojis]
+        scopes = ["feat", "fix", "docs", "style", "refactor", "perf", "test", "build", "ci", "chore", "revert"]
         pattern = r"^({:s})\(.*\)\s[a-zA-Z].*$".format("|".join(emojis))
         if not re.search(pattern, title):
             violation_msg = 'Title does not match regex "<gitmoji>(<scope>) <subject>"'
+            return [RuleViolation(self.id, violation_msg, title)]
+
+
+        pattern = r"^({:s})\({:s}\)\s[a-zA-Z].*$".format("|".join(emojis),"|".join(scopes) )
+        if not re.search(pattern, title):
+            violation_msg = 'The scope should be one of: {:s}'.format(", ".join(scopes))
             return [RuleViolation(self.id, violation_msg, title)]
